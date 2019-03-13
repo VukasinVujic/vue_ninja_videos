d4c485a8823e8645ad63158f0e468525797f750b
VukasinVujic <vukasinvujic021@gmail.com>
2019-03-13 11:35:17 +0100

    video 4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,14 @@
 
 <div>
 
-  <h1> {{greet("afternoon")}}  </h1>
- <p>Name: {{name}}</p>
-  <p>Job: {{job}} </p>
+<h1>Data binding</h1>
+  <!-- <h1> {{greet("afternoon")}}  </h1> -->
+ <a v-bind:href="website"> link za sa sajt</a>
+ <!-- skracenica dole od ovog gore -->
+ <!--  <a :href="website"> link za sa sajt</a> -->
+<input type="text" v-bind:value="job">
 
+  <p v-html="websiteTag"></p>
 
 </div>
 </template>
@@ -16,7 +20,8 @@ export default {
     return{
       name:"Shaun",
       job:"arbeit",
-
+      website: "https://www.google.rs/",
+      websiteTag: '<a href="https://www.google.rs/"> aaaa </a> '
     };
   },
   
